3c380cc478410fb82903a35b7c062a97b4b4b904
brandondando <dandobrandon0@gmail.com>
2024-07-06 06:06:07 +0300

    Complete

--- server/app.py
+++ server/app.py
@@ -18,14 +18,12 @@ api = Api(app)
 
 
 class Plants(Resource):
-
     def get(self):
         plants = [plant.to_dict() for plant in Plant.query.all()]
         return make_response(jsonify(plants), 200)
-
+    
     def post(self):
         data = request.get_json()
-
         new_plant = Plant(
             name=data['name'],
             image=data['image'],
@@ -34,19 +32,27 @@ class Plants(Resource):
 
         db.session.add(new_plant)
         db.session.commit()
-
         return make_response(new_plant.to_dict(), 201)
-
-
 api.add_resource(Plants, '/plants')
-
-
 class PlantByID(Resource):
-
     def get(self, id):
-        plant = Plant.query.filter_by(id=id).first().to_dict()
+        plant = Plant.query.filter_by(id = id).first().to_dict()
         return make_response(jsonify(plant), 200)
 
+    def patch(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        data = request.get_json()
+        plant.is_in_stock = data.get("is_in_stock", plant.is_in_stock) 
+        db.session.commit()
+
+        return make_response(plant.to_dict(), 200)
+    
+    def delete(self, id):
+        plant = Plant.query.filter_by(id = id).first()
+        db.session.delete(plant)
+        db.session.commit()
+
+        return make_response('deleted plant successfully', 204)
 
 api.add_resource(PlantByID, '/plants/<int:id>')
 
